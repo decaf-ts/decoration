e9f6c19d5e427dba4250d2ced9bf5357ff3dc32f
tvenceslau <d3caf.ts@gmail.com>
2025-11-07 19:26:04 +0000

    DECAF-178 trying out new solutions

---
+++ tests/unit/compatibility.test.ts
@@ -0,0 +1,99 @@
+import { Decoration, metadata, Metadata } from "../../src/index";
+
+describe("Multiple Decoration Compatibility", () => {
+  it("Applies multiple decorations without conflict", () => {
+    const f1 = jest.fn();
+    const f2 = jest.fn();
+    const f3 = jest.fn();
+
+    function decorator(name: string, num: number) {
+      function innerDecorator(name: string, num: number) {
+        return function (
+          target: any,
+          propertyKey?: string | symbol,
+          descriptor?: PropertyDescriptor
+        ) {
+          return f1(name, num, target, propertyKey, descriptor);
+        };
+      }
+
+      return Decoration.for("TEST")
+        .define({
+          decorator: innerDecorator,
+          args: [name, num],
+        })
+        .apply();
+    }
+
+    function decorator2(name: string, num: number) {
+      return function (
+        target: any,
+        propertyKey?: string | symbol,
+        descriptor?: PropertyDescriptor
+      ) {
+        return f2(name, num, target, propertyKey, descriptor);
+      };
+    }
+
+    function decorator3(name: string, num: number) {
+      return function (
+        target: any,
+        propertyKey?: string | symbol,
+        descriptor?: PropertyDescriptor
+      ) {
+        return f3(name, num, target, propertyKey, descriptor);
+      };
+    }
+
+    Decoration.flavouredAs("2")
+      .for("TEST")
+      .define({
+        decorator: decorator2,
+      } as any)
+      .apply();
+
+    Decoration.flavouredAs("3")
+      .for("TEST")
+      .define({
+        decorator: decorator3,
+      } as any)
+      .apply();
+
+    Decoration.setFlavourResolver((obj: object) => {
+      const meta = Metadata.get(obj.constructor as any);
+      return meta ? meta["META"] : undefined;
+    });
+
+    class Obj1 {
+      @decorator("first", 1)
+      prop!: string;
+      constructor() {}
+    }
+
+    @metadata("META", "2")
+    class Obj2 {
+      @decorator("first", 2)
+      prop!: string;
+      constructor() {}
+    }
+
+    @metadata("META", "3")
+    class Obj3 {
+      @decorator("first", 3)
+      prop!: string;
+      constructor() {}
+    }
+
+    const obj1 = new Obj1();
+    obj1.prop = "test1";
+    expect(f1).toHaveBeenCalledWith("first", 1, {}, "prop", undefined);
+
+    const obj2 = new Obj2();
+    obj2.prop = "test2";
+    expect(f2).toHaveBeenCalledWith("first", 2, {}, "prop", undefined);
+
+    const obj3 = new Obj3();
+    obj3.prop = "test3";
+    expect(f3).toHaveBeenCalledWith("first", 3, {}, "prop", undefined);
+  });
+});
